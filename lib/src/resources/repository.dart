e2f36ad568dbdfff3a67bee5b83b71fc4a66d6f3
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2020-03-26 19:49:01 +0530

    init

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'news_api_provider.dart';
+import 'news_db_provider.dart';
+import '../models/item_model.dart';
+
+class Repository{
+  NewsDBProvider dbProvider = new NewsDBProvider();
+  NewsApiProvider apiProvider = new NewsApiProvider();
+
+  Future<List<int>> fetchTopIds(){
+    return apiProvider.fetchTopIds();
+  }
+
+  Future<ItemModel> fetchItem(int id) async{
+    var item = await dbProvider.fetchItem(id);
+    
+    if(item != null){
+      return item;
+    }
+
+    item = await apiProvider.fetchItem(id);
+
+    dbProvider.addItem(item);
+
+    return item;
+  }
+
+}
