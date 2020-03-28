0f264020484afa4b0f9baafae239617867eea02d
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2020-03-28 18:07:34 +0530

    steam transform

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
+import 'bloc/stories_provider.dart';
 
 class App extends StatelessWidget {
-  Widget build(context){
-    return MaterialApp(
-      title: 'News!',
-      home : NewsList()
+  Widget build(context) {
+    return StoriesProvider(
+      child: MaterialApp(
+        title: 'News!',
+        home: NewsList(),
+      ),
     );
   }
 }
--- lib/src/bloc/stories_provider.dart
+++ lib/src/bloc/stories_provider.dart
@@ -2,17 +2,15 @@ import 'package:flutter/material.dart';
 import 'stories_bloc.dart';
 
 class StoriesProvider extends InheritedWidget {
-    final StoriesBloc bloc;
+  final StoriesBloc bloc;
 
-    StoriesProvider({Key key, Widget child})
+  StoriesProvider({Key key, Widget child})
       : bloc = StoriesBloc(),
-        super(key : key, child : child);
+        super(key: key, child: child);
 
-    bool updateShouldNotify(_) => true;
+  bool updateShouldNotify(_) => true;
 
-    static StoriesBloc of(BuildContext context) {
-      return (context.inheritFromWidgetOfExactType(StoriesProvider)
-               as StoriesProvider)
-               .bloc; 
-    }
+  static StoriesBloc of(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType<StoriesProvider>()).bloc;
+  }
 }
